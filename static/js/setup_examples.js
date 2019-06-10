c4ce2a3900fa518dc1613c5c18df085bd1d806bb
martinSergeant <m.j.sergeant1@googlemail.com>
2019-06-10 09:26:55 +0100

    more examples

---
+++ static/js/setup_examples.js
@@ -0,0 +1,39 @@
+function setupExamples(json_file,card_deck){
+
+	$.ajax({
+		type:"GET",
+		"dataType":"json",
+		"url":json_file
+
+	}).done(function(data){
+		var deck = $("#"+card_deck)
+		for (var i in data){
+			var item = data[i];
+			var card =$("<div>").attr("class","card mlv-card");
+			var header = $("<div>").attr("class","card-header").html("<h6>"+item.id+". "+item.title+"</h6>");
+			var body = $("<div>").attr("class","card-body")
+			body.append("<p class='card-text'>"+item.desc+"</p>");
+			var footer = $("<div>").attr("class","card-footer").appendTo(card);
+			let bt_class = "btn btn-secondary btn-sm float-left";
+			if (item.disable_view){
+				bt_class+=" disabled";
+			}
+			$("<a>").attr({
+						"class":bt_class,
+						"href":"https://martinsergeant.github.io/examples/example"+item.id+".html"
+						}).text("view").appendTo(footer)
+			$("<a>").attr({
+						"class":"btn btn-secondary btn-sm float-right",
+						"href":"https://github.com/Hughes-Genome-Group/MLVPanel/blob/master/examples/example"+item.id+".html"
+						}).text("source").appendTo(footer)
+			
+			card.append(header).append(body).append(footer).appendTo(deck);
+
+
+		}
+
+	})
+
+
+
+}
